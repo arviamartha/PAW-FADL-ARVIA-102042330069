670d7c387fc124e0cc0474fc14d4b92cb8f9f179
arviamrtha <arviamarthina5@gmail.com>
2025-05-02 14:20:53 +0700

    Update Jurnal

---
+++ JURNAL3_ARVIA/app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    public function index()
+    {
+        // ==================2==================
+        // - Buat object mahasiswa dengan data dummy (nama, nim, email, jurusan, fakultas, foto)
+        // - Kirim object tersebut ke view 'profil'
+    }
+}
